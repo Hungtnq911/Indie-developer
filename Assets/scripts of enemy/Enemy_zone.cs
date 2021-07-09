a43d43f96c3c8441820911189f084e2a3db97999
nomanlike <57402905+nomanlike@users.noreply.github.com>
2021-07-09 09:38:39 +0700

    a bit of change

--- Assets/scripts of enemy/Enemy_zone.cs
+++ Assets/scripts of enemy/Enemy_zone.cs
@@ -7,7 +7,7 @@ public class Enemy_zone : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        Vector3 raycastDirection = Vector3.up;
+        Vector3 raycastDirection = Vector3.down;
         float mRaycastingDistance = 1f;
         RaycastHit2D hit = Physics2D.Raycast(transform.position + raycastDirection * mRaycastingDistance - new Vector3(0f, 0.25f, 0f), raycastDirection, 0.075f);
         if (hit.collider!= null)
@@ -28,7 +28,7 @@ public class Enemy_zone : MonoBehaviour
     public bool Grabb()
     {
         
-            return true;
+            return hey;
     }
 
 }
