f983584b00a0eee3299a522ea602d70a899f2ba9
tnqhung <trannguyenquochung01@gmail.com>
2021-07-09 15:31:49 +0700

    animation,animator

--- Assets/scripts of enemy/upgraded_length_patroller.cs
+++ Assets/scripts of enemy/upgraded_length_patroller.cs
@@ -10,6 +10,7 @@ public class upgraded_length_patroller : MonoBehaviour
     private SpriteRenderer _mSpriteRenderer;
     public float length_timer = 6;
     // Start is called before the first frame update
+<<<<<<< HEAD
     public string id = "ABC";
     private bool flipstate = false;
     private enum State
@@ -22,19 +23,19 @@ public class upgraded_length_patroller : MonoBehaviour
     {
         state = State.patrol;
     }
+=======
+>>>>>>> parent of bc1c1eb (it hurts)
     void Start()
     {
         _mSpriteRenderer = gameObject.GetComponent<SpriteRenderer>();
         _mSpriteRenderer.flipX = !bIsGoingRight;
-
-
-
     }
 
 
     void Update()
     {
         
+<<<<<<< HEAD
         float k = mMovementSpeed;
         Vector3 directionTranslation = (bIsGoingRight) ? transform.right : -transform.right;
 
@@ -57,9 +58,21 @@ public class upgraded_length_patroller : MonoBehaviour
                     // if the ennemy is going right, get the vector pointing to its right
 
                     mMovementSpeed = k;
+=======
 
-                    directionTranslation *= Time.deltaTime * mMovementSpeed;
-                    transform.Translate(directionTranslation);
+
+        // if the ennemy is going right, get the vector pointing to its right
+
+            Vector3 directionTranslation = (bIsGoingRight) ? transform.right : -transform.right;
+
+            Vector3 raycastDirection = (bIsGoingRight) ? Vector3.right : Vector3.left;
+
+            directionTranslation *= Time.deltaTime * mMovementSpeed;
+>>>>>>> parent of bc1c1eb (it hurts)
+
+            RaycastHit2D hit = Physics2D.Raycast(transform.position + raycastDirection * mRaycastingDistance - new Vector3(0f, 0.25f, 0f), raycastDirection, 0.075f);
+            
+            transform.Translate(directionTranslation);
 
             if (hit.collider != null)
             {
@@ -70,28 +83,31 @@ public class upgraded_length_patroller : MonoBehaviour
 
                 }
             }
-            if (length_timer >0)
-            StartCoroutine("delay");
-            
-        }; break;
 
+<<<<<<< HEAD
             case State.shooting:
                 {
                     mMovementSpeed = 0;
                     directionTranslation *= Time.deltaTime * mMovementSpeed;
                     transform.Translate(directionTranslation);
-
-                };break;
-
-        }
-
+=======
+  StartCoroutine("delay");
         
+       
+     
+>>>>>>> parent of bc1c1eb (it hurts)
 
+
+
+
+<<<<<<< HEAD
       
 
         Enemy_zone z= GameObject.Find(id).GetComponent<Enemy_zone>();
         flipstate = z.GetComponent<Enemy_zone>().Grabb();
             
+=======
+>>>>>>> parent of bc1c1eb (it hurts)
         // for (int i=0;i<5;i++)
 
 
@@ -109,7 +125,5 @@ public class upgraded_length_patroller : MonoBehaviour
         StopCoroutine("delay");
         //StopAllCoroutines();
     }
-
-   
 
 }
