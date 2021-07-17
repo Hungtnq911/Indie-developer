0e235ea5f7f68b6438d860a2a976186168c21ad1
nomanlike <57402905+nomanlike@users.noreply.github.com>
2021-07-17 20:27:11 +0700

    update

--- Assets/scripts of enemy/player_sensor.cs
+++ Assets/scripts of enemy/player_sensor.cs
@@ -5,16 +5,18 @@ using UnityEngine;
 public class player_sensor : MonoBehaviour
 {
     // Start is called before the first frame update
-    public int k = 0;
+
     // Update is called once per frame
     private void OnTriggerEnter2D(Collider2D collision)
     {
+        if (collision.gameObject.CompareTag("zone"))
             collision.GetComponent<Enemy_zone>().enabled = true;
         
     }
 
     private void OnTriggerExit2D(Collider2D collision)
     {
-        collision.GetComponent<Enemy_zone>().enabled = false;
+        if (collision.gameObject.CompareTag("zone"))
+            collision.GetComponent<Enemy_zone>().enabled = false;
     }
 }
